18bda4b12243a73790ab7b4d40e8d928d7237a7a
cj <sigamateusz@gmail.com>
2017-04-22 18:15:08 +0200

    added text fields for login & password

--- app/src/main/java/com/example/cj/cracowsightseeing/MainActivity.java
+++ app/src/main/java/com/example/cj/cracowsightseeing/MainActivity.java
@@ -3,21 +3,33 @@ package com.example.cj.cracowsightseeing;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity implements View.OnClickListener {
     Button button;
+    private EditText login;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         button = (Button) findViewById(R.id.button);
+        login = (EditText) findViewById(R.id.login);
+        password = (EditText) findViewById(R.id.password);
         button.setOnClickListener(this);
     }
 
     private void buttonClick() {
+        String out_login = login.getText().toString();
+        String out_password = password.getText().toString();
+
+        Log.i("Login: ", out_login);
+        Log.i("Pass: ", out_password);
+
         startActivity(new Intent("MapsActivity"));
     }
 
