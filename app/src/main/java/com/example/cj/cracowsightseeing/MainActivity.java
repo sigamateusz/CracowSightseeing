3aeef7bff603d9f4a4aee5d3b78c9bc2497f11da
cj <sigamateusz@gmail.com>
2017-04-22 23:12:17 +0200

    sends & gets JSONobject

--- app/src/main/java/com/example/cj/cracowsightseeing/MainActivity.java
+++ app/src/main/java/com/example/cj/cracowsightseeing/MainActivity.java
@@ -1,17 +1,30 @@
 package com.example.cj.cracowsightseeing;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends Activity implements View.OnClickListener {
     Button button;
     private EditText login;
     private EditText password;
+    String server_url = "http://192.170.21.107:5000/android/login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +40,19 @@ public class MainActivity extends Activity implements View.OnClickListener {
         String out_login = login.getText().toString();
         String out_password = password.getText().toString();
 
-        Log.i("Login: ", out_login);
-        Log.i("Pass: ", out_password);
-
-        startActivity(new Intent("MapsActivity"));
+//        Log.i("Login: ", out_login);
+//        Log.i("Pass: ", out_password);
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("login", out_login);
+            jsonObject.put("password", out_password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        makeJsonObjReq(jsonObject);
+        Toast.makeText(this, "Wrong login or password",
+                Toast.LENGTH_SHORT).show();
+//        startActivity(new Intent("MapsActivity"));
     }
 
     public void onClick(View v) {
@@ -39,5 +61,55 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 buttonClick();
                 break;
         }
+    }
+
+    private void makeJsonObjReq(JSONObject jsonObject) {
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                server_url, jsonObject,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            if (response.getBoolean("status")) {
+                                Log.i("Response: ", response.toString());
+                            } else {
+                                Log.i("Response: ", "False");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("Error: ", error.getMessage());
+            }
+        }) {
+
+            /**
+             * Passing some request headers
+             * */
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("un", "xyz@gmail.com");
+                params.put("p", "somepasswordhere");
+                return params;
+            }
+
+        };
+
+        String isNull = ApplicationController.getInstance() == null ? " jest nulem" : "jest ok";
+        Log.d("aa", "makeJsonObjReq: " + isNull);
+        ApplicationController.getInstance().addToRequestQueue(jsonObjReq);
     }
 }
---
+++ app/src/main/java/com/example/cj/cracowsightseeing/ServerConnection.java
@@ -0,0 +1,6 @@
+package com.example.cj.cracowsightseeing;
+
+
+public class ServerConnection {
+
+}
