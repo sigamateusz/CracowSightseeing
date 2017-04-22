4809c470a72c8df950281b55aee7cc9140c4901a
cj <sigamateusz@gmail.com>
2017-04-22 23:51:21 +0200

    added nick, level & score to Menu

--- app/src/main/java/com/example/cj/cracowsightseeing/Menu.java
+++ app/src/main/java/com/example/cj/cracowsightseeing/Menu.java
@@ -3,9 +3,11 @@ package com.example.cj.cracowsightseeing;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Menu extends Activity implements View.OnClickListener {
     Button mapBt;
@@ -14,10 +16,24 @@ public class Menu extends Activity implements View.OnClickListener {
     Button quiz;
     Button logout;
 
+    TextView playerLevel;
+    TextView experience;
+    TextView nick;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+
+        playerLevel = (TextView) findViewById(R.id.playerLevel);
+        experience = (TextView) findViewById(R.id.experience);
+        nick = (TextView) findViewById(R.id.nick);
+
+        Log.i("Name: ", ApplicationController.user.getName());
+        playerLevel.setText(Integer.toString(ApplicationController.user.getLevel()));
+        experience.setText(Integer.toString(ApplicationController.user.getScore()));
+        nick.setText(ApplicationController.user.getName());
+
         mapBt = (Button) findViewById(R.id.mapBt);
         beacon = (Button) findViewById(R.id.beacon);
         leaderboard = (Button) findViewById(R.id.leaderboard);
